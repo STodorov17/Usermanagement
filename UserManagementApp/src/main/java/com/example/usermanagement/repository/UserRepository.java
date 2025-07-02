8cefa371dfec35b8b6640e731a99fd81c103442e
STodorov <137818757+STodorov17@users.noreply.github.com>
2025-07-02 21:58:36 +0300

    Delete UserManagementApp directory

--- UserManagementApp/src/main/java/com/example/usermanagement/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.usermanagement.repository;
-
-import com.example.usermanagement.entity.User;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	List<User> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
-}
